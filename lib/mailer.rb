6f3531c0a405bfb1f17ed85b0a3d49e8e84de795
Jarmo Isotalo <jamo@isotalo.fi>
2014-10-05 21:53:14 +0200

    Added logic for sending notification emails

---
+++ lib/mailer.rb
@@ -0,0 +1,27 @@
+require 'action_mailer'
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              => "mail.cs.helsinki.fi",
+  :port                 => 587,
+  :domain               => 'cs.helsinki.fi',
+  :user_name            => ENV["SMTP_USERNAME"],
+  :password             => ENV["SMTP_PASSWORD"],
+  :authentication       => 'login',
+  :enable_starttls_auto => true
+}
+ActionMailer::Base.view_paths = File.dirname(__FILE__)
+puts File.dirname(__FILE__)
+
+class Mailer < ActionMailer::Base
+
+  def alert(email, body)
+    @body = body
+    mail( :to      => email,
+         :from    => "TMC PROBER <jarmoiso@cs.helsinki.fi>",
+         :subject => "subject") do |format|
+           format.html
+         end
+  end
+end
